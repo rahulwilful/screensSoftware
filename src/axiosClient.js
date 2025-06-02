48928284608d3e45cbc367f174fa7c5036a6dae3
rahul patil <rahulwilful@gmail.com>
2025-06-02 22:08:13 +0530

    02/06/2025

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosClient = axios.create({
   //baseURL: 'https://digitalhr-web-service.onrender.com/',
-  baseURL: "http://localhost:5001/api/v1/",
+  baseURL: 'http://localhost:5001/api/v1/'
 });
 // Add a request interceptor to set the Authorization header before each request
 axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+  config => {
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
   }
 );
